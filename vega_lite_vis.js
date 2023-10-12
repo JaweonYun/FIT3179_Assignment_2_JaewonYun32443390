8f6a4f2aab214c0b5ecec39b316ab1879140b1a2
JaweonYun <89090618+JaweonYun@users.noreply.github.com>
2023-10-12 15:46:04 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "stacked_chart.vg.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2 = "stacked_chart_AVGOECD.json";
+vegaEmbed("#bar_chart_2", vg_2).then(function(result) {
+}).catch(console.error);
+
